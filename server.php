00b2aec8ccdf7097592855d3446c5d4244821656
zihan <zihan.yx@taobao.com>
2013-04-16 20:19:19 +0800

    update

--- server.php
+++ server.php
@@ -1,23 +1,32 @@
 <?php
-$id = $_GET['index'];
-switch ($id) {
+$index = $_GET["index"];
+switch ($index) {
 	case 0:
-		echo '{"id": "0", "name": "太阳"}';
+		echo '{"id": "0", "name": "路飞", "url": "./assets/op/11.jpg"}';
 		break;
 	case 1:
-		echo '{"id": "1", "name": "萝卜"}';
+		echo '{"id": "1", "name": "索隆", "url": "./assets/op/22.jpg"}';
 		break;
 	case 2:
-		echo '{"id": "2", "name": "便便"}';
+		echo '{"id": "2", "name": "娜美", "url": "./assets/op/33.jpg"}';
 		break;
 	case 3:
-		echo '{"id": "3", "name": "草鸡"}';
+		echo '{"id": "3", "name": "乔巴", "url": "./assets/op/44.jpg"}';
 		break;
 	case 4:
-		echo '{"id": "4", "name": "灰机"}';
+		echo '{"id": "4", "name": "弗兰克", "url": "./assets/op/55.jpg"}';
 		break;
 	case 5:
-		echo '{"id": "5", "name": "五叶草"}';
+		echo '{"id": "5", "name": "布鲁克", "url": "./assets/op/66.jpg"}';
 		break;
-}
+	case 6:
+		echo '{"id": "6", "name": "罗宾", "url": "./assets/op/77.jpg"}';
+		break;
+	case 7:
+		echo '{"id": "7", "name": "乌索普", "url": "./assets/op/88.jpg"}';
+		break;
+	case 8:
+		echo '{"id": "8", "name": "香吉士", "url": "./assets/op/99.jpg"}';
+		break;
+	} 
 ?>
